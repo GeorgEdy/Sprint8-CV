e010a4b4b8c81c1f714e75a243870b0ff88bda4a
razvan393 <razvanghinea05@gmail.com>
2016-03-02 10:53:42 +0200

    Save/Cancel buttons v1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the sprint8App
  */
 angular.module('sprint8App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.displayButtons = function (event) {
+      $(event.target.parentNode).removeClass('hide-buttons');
+    };
+
+    $scope.cancelAction = function () {
+      $(event.target.parentNode).addClass('hide-buttons');
+    };
+
+    $scope.saveAction = function () {
+      $(event.target.parentNode).addClass('hide-buttons');
+    };
+
   });
