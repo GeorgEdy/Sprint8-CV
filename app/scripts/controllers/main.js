5b6b6e4ae54d83b58a9fdc7d5df9acc6220ddb28
panaiteandreisilviu <andrei_silviu.panaite@aut.pub.ro>
2016-03-03 02:05:23 +0200

    Added datePicker directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,19 +23,25 @@
       {
         position: 'Junior Programmer',
         company: 'Umbrella Corporation',
-        description: 'Programming killer software'
+        description: 'Programming killer software',
+        dateStart: '2010-05-10',
+        dateEnd: '2014-05-10'
       },
       {
         position: 'Manager Assistant',
         company: 'Red Box',
-        description: 'No ideea what I am doing here'
+        description: 'No ideea what I am doing here',
+        dateStart: '2014-05-10',
+        dateEnd: '2015-05-10'
       }
     ];
     $scope.education = [
       {
         degree: 'BAC',
         school: 'Numaru 9',
-        description: 'best school ever'
+        description: 'best school ever',
+        dateStart: '2014-05-10',
+        dateEnd: '2015-05-10'
       }
     ];
     $scope.languages = [
@@ -93,6 +99,34 @@
         deleteField: deleteField
       }
     });
+  });
+
+  app.directive('datePicker', function ($compile, $templateCache) {
+    return {
+      replace: true,
+      transclude: true,
+      template: '<span><span ng-include="template"></span></span>',
+      scope: {
+        dateValue: '@dateValue'
+      },
+      link: function (scope, element, attributes) {
+        scope.dateValue = attributes.date;
+        attributes.$observe('show', function (value) {
+          //scope.template = (value === true) ? : 'views/spanDate.html';
+          if (value === true) {
+            $compile(element.contents())(scope);
+            scope.template = 'views/input.html';
+          } else {
+            $compile(element.contents())(scope);
+            scope.template = 'views/spanDate.html';
+          }
+
+          $compile(element.contents())(scope);
+          console.log(value, scope.template);
+        });
+
+      }
+    };
   });
 
 })();
