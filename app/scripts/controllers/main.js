7bff8fa9f846553816d3e107605416c2b82c33c6
razvan393 <razvanghinea05@gmail.com>
2016-03-03 12:11:10 +0200

    Reworked local data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,22 +4,23 @@
   var app = angular.module('sprint8App');
 
   //controllers
-  app.controller('MainCtrl', function ($scope, FieldActions) {
+  app.controller('MainCtrl', function ($scope) {
 
     $scope.showButtons = {work: false, education: false, languages: false, proSkills: false};
-    $scope.cvName = 'Untitled CV';
-    $scope.personalDetails = {
-      name: 'Ghinea Razvan',
-      currPos: '',
-      aboutMe: 'Je suis smecher',
-      phoneNumber: '',
-      address: 'Cucuietii din deal',
-      email: '',
-      website: '',
-      skype: '',
-      linkedin: ''
-    };
-    $scope.workingExperience = [
+    $scope.data = {
+      cvName: 'Untitled CV',
+      personalDetails: {
+        name: 'Gogu Caldararu',
+        currPos: '',
+        aboutMe: 'Je suis smecher',
+        phoneNumber: '',
+        address: 'Cucuietii din deal',
+        email: '',
+        website: '',
+        skype: '',
+        linkedin: ''
+      },
+      workingExperience: [
       {
         position: 'Junior Programmer',
         company: 'Umbrella Corporation',
@@ -30,70 +31,62 @@
         company: 'Red Box',
         description: 'No ideea what I am doing here'
       }
-    ];
-    $scope.education = [
+    ],
+      education: [
       {
         degree: 'BAC',
         school: 'Numaru 9',
         description: 'best school ever'
       }
-    ];
-    $scope.languages = [
+    ],
+      languages: [
       {
         name: 'english',
         understanding: 'C1',
         speaking: 'B2',
         writing: 'B2'
       }
-    ];
-    $scope.skills = 'smecherie';
+    ],
+      skills: 'smecherie'
+  };
 
     $scope.addField = function (name) {
-      //FieldActions.addField($scope, name);
       if (name === 'workingExperience') {
-        $scope.workingExperience.push({
+        $scope.data.workingExperience.push({
           position: '',
           company: '',
           description: ''
+        })
+      } else if (name === 'education') {
+        $scope.data.education.push({
+          degree: '',
+          school: '',
+          description: ''
+        })
+      } else {
+        $scope.data.languages.push({
+          name: '',
+          understanding: '',
+          speaking: '',
+          writing: ''
         })
       }
     };
 
     $scope.deleteField = function (name, event) {
       var index = $(event.target).data('id');
+      console.log(index);
       if (name === 'workingExperience') {
-        $scope.workingExperience.splice(index, 1);
+        $scope.data.workingExperience.splice(index, 1);
+      } else if (name === 'education') {
+        $scope.data.education.splice(index, 1);
+      } else {
+        $scope.data.languages.splice(index, 1);
       }
     };
 
   });
 
-  //services
-
-  app.factory('FieldActions', function () {
-    return (function () {
-      var addField = function ($scope, name) {
-        if (name === 'workingExperience') {
-          $scope.workingExperience.push({
-            position: '',
-            company: '',
-            description: ''
-          });
-        }
-      };
-
-      var deleteField = function ($scope, name, index) {
-        if (name === 'workingExperience') {
-          $scope.workingExperience.splice(index, 1);
-        }
-      };
-
-      return {
-        addField: addField,
-        deleteField: deleteField
-      }
-    });
-  });
 
 })();
 
